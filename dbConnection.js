bddd105670661d2a77b01297a4dc5f8124d92d55
Agnesnia <agnesniaviyane04@gmail.com>
2024-11-25 20:46:52 +0900

    Add files via upload

---
+++ dbConnection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', 
+    password: '', 
+    database: 'biodata' 
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+module.exports = { db };
+
+
+    
